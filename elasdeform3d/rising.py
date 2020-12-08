e32f983d4d0aa7d7b0e0c55368e482a1211c6eb4
Patrick M. Jensen <patmjen@dtu.dk>
2020-12-08 12:32:42 +0100

    Allow separate interp. mode per key in elas. def.
    The motivation is that we likely want linear interpolation for data
    but nearest neighbor interpolation for labels.

--- elasdeform3d/rising.py
+++ elasdeform3d/rising.py
@@ -1,11 +1,10 @@
 from rising.transforms.abstract import BaseTransformSeeded
 from .deform import elastic_deform_3d
-from .deform import ElasticDeformer3d as BaseElasticDeformer3d
 
 
 class ElasticDeformer3d(BaseTransformSeeded):
     """
-    Simple wrapper class which exposes the ElasticDeformer3d class as a seeded
+    Simple wrapper class which exposes the elastic_deforma_3d as a seeded
     rising transform.
     """
 
@@ -22,8 +21,35 @@ class ElasticDeformer3d(BaseTransformSeeded):
             Standard deviation of Gaussian distribution for displacement
             vectors.
         interp_mode
-            Interpolation mode. Must be 'nearest' (default) or 'linear'.
+            Interpolation mode or dict mapping keys to interpolations mode.
+            Modes must be 'nearest' (default) or 'linear'.
         """
-        super().__init__(
-            augment_fn=BaseElasticDeformer3d(grid_spacing, std, interp_mode),
-            keys=keys)
+        super().__init__(augment_fn=self.deform, keys=keys)
+        self.grid_spacing = grid_spacing
+        self.std = std
+        if isinstance(interp_mode, str):
+            self.interp_modes = { k: interp_mode for k in keys }
+        else:
+            self.interp_modes = interp_mode
+
+
+    def deform(self, data_and_key):
+        data, key = data_and_key
+        return elastic_deform_3d(data, self.grid_spacing, self.std,
+            interp_mode=self.interp_modes[key])
+
+
+    def forward(self, **data):
+        """
+        Apply transformation and use same seed for every key
+
+        Parameters
+        ----------
+            data: dict with tensors
+
+        Returns
+        -------
+            dict: dict with augmented data
+        """
+        # Add the key to each entry before sending it further
+        return super().forward(**{ k: (d, k) for k, d in data.items() })

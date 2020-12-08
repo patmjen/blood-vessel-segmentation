439922dc7535ca5a626d193d34c98cd249ad73b5
Patrick M. Jensen <patmjen@dtu.dk>
2020-12-08 17:28:02 +0100

    Use scheduler to reduce learning rate on plateu

--- vnet.py
+++ vnet.py
@@ -272,7 +272,8 @@ class VNet(pl.LightningModule):
 
     def configure_optimizers(self):
         optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams.lr)
-        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.hparams.max_epochs)
-        # return [optimizer], [scheduler]
-        return optimizer
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                               'min')
+        return { 'optimizer': optimizer, 'lr_scheduler': scheduler,
+                 'monitor': 'val_loss' }
 
